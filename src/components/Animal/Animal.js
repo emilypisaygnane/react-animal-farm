e84b83efb8c2b78093e7b3b001f1d36ff8cf7d3f
Emily Pisaygnane <episaygnane@me.com>
2022-09-12 14:47:25 -0700

    added background.png to main.js

--- src/components/Animal/Animal.js
+++ src/components/Animal/Animal.js
@@ -1,3 +1,7 @@
-export default function Animal() {
-  return <></>;
+import React from 'react';
+import './Animal.css';
+
+export default function Animal({ name, says, top, left, type }) {
+  return (
+  <></>;
 }
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,6 +1,16 @@
 import React from 'react';
 import './Main.css';
+import backgroundImg from '../../background.png';
+import Animal from '../Animal/Animal.js';
+import { animals } from '../../data.js';
+
 
 export default function Main() {
-  return <main></main>;
+  return (
+    <main style={{ backgroundImage: `url(${backgroundImg})` }}>
+      {animals.map((item) => (
+        <Animal key={item.name} {...item} />
+      ))}
+    </main>
+  );
 }
